9ff88bc92dd1e0756f4628391249e477524955ae
Ruben Bisharyan <ruben.bisharyan@pandatech.it>
2024-03-21 15:28:05 +0400

    Add overload with error messages for TooManyRequestsException

--- src/ResponseCrafter/StandardHttpExceptions/TooManyRequestsException.cs
+++ src/ResponseCrafter/StandardHttpExceptions/TooManyRequestsException.cs
@@ -1,6 +1,16 @@
 ﻿namespace ResponseCrafter.StandardHttpExceptions;
 
-public class TooManyRequestsException(string message = TooManyRequestsException.DefaultMessage) : ApiException(429, message)
+public class TooManyRequestsException : ApiException
 {
     private const string DefaultMessage = "you_have_sent_too_many_requests_in_a_given_amount_of_time._please_try_again_later.";
+    
+    public TooManyRequestsException(string message = DefaultMessage)
+        : base(429, message)
+    {
+    }
+    
+    public TooManyRequestsException(string message, Dictionary<string, string>? errors = null)
+        : base(429, message, errors)
+    {
+    }
 }
